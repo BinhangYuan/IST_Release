657ccc7a385d79c04ac465f25b63606943d917bb
Binhang Yuan <yuanbinhang@hotmail.com>
2020-12-08 18:08:11 +0800

    Release

--- google_speech_data_loader.py
+++ google_speech_data_loader.py
@@ -35,7 +35,7 @@ def which_set(filename, testing_percentage=20):
     return result
 
 
-def pre_processing_dataset(data_path='./data_speech_commands_v0.02/'):
+def pre_processing_dataset(data_path='./data_speech_commands_v0.02/', transform=None):
     columns = ['wav', 'label_str']
     train_df = pd.DataFrame(columns=columns)
     test_df = pd.DataFrame(data=np.zeros((0, len(columns))), columns=columns)
@@ -56,17 +56,47 @@ def pre_processing_dataset(data_path='./data_speech_commands_v0.02/'):
                     train_df = train_df.append({'wav': full_path, 'label_str': x}, ignore_index=True)
     train_df.to_csv(path_or_buf=data_path+'training_set.csv', index=False)
     test_df.to_csv(path_or_buf=data_path+'test_set.csv', index=False)
+    train_buffer_x = []
+    train_buffer_y = []
+    for index in tqdm(range(len(train_df))):
+        wav_name = train_df.iloc[index, 0]
+        wav_sample_rate, wav = wf.read(wav_name)
+        sample = GoogleSpeechDataset.mel_log(wav, wav_sample_rate).astype('float')
+        if transform:
+            sample = transform(sample)
+        train_buffer_x.append(sample)
+        train_buffer_y.append(wanted_words_to_class[train_df.iloc[index, 1]])
+    train_x = np.concatenate(train_buffer_x, axis=0)
+    train_y = np.array(train_buffer_y)
+    np.save(data_path+'train_x.npy', train_x)
+    np.save(data_path+'train_y.npy', train_y)
+    test_buffer_x = []
+    test_buffer_y = []
+    for index in tqdm(range(len(test_df))):
+        wav_name = test_df.iloc[index, 0]
+        wav_sample_rate, wav = wf.read(wav_name)
+        sample = GoogleSpeechDataset.mel_log(wav, wav_sample_rate).astype('float')
+        if transform:
+            sample = transform(sample)
+        test_buffer_x.append(sample)
+        test_buffer_y.append(wanted_words_to_class[test_df.iloc[index, 1]])
+    test_x = np.concatenate(test_buffer_x, axis=0)
+    test_y = np.array(test_buffer_y)
+    np.save(data_path+'test_x.npy', test_x)
+    np.save(data_path+'test_y.npy', test_y)
 
 
 class GoogleSpeechDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None, load_cached=False):
-        self.wav_frame = pd.read_csv(csv_file)
+    def __init__(self, input_file, root_dir, transform=None, load_cached=False):
+        self.wav_frame = pd.read_csv(input_file)
         self.root_dir = root_dir
         self.transform = transform
+        self.load_cached = load_cached
         print('Pin google speech recognition dataset in the RAM.')
         start_time = time.time()
         if load_cached:
-            pass
+            self.buffer_x = np.load(input_file+'_x.npy')
+            self.buffer_y = np.load(input_file + '_y.npy')
         else:
             self.buffer = []
             for index in tqdm(range(len(self.wav_frame))):
@@ -85,7 +115,10 @@ class GoogleSpeechDataset(Dataset):
         return len(self.buffer)
 
     def __getitem__(self, index):
-        return self.buffer[index]
+        if self.load_cached:
+            return {'wav': self.buffer_x[i], 'label': self.buffer_y[i]}
+        else:
+            return self.buffer[index]
 
     @staticmethod
     def mel_log(audio, sample_rate):
@@ -95,11 +128,11 @@ class GoogleSpeechDataset(Dataset):
 
 
 def train_dataset():
-    return GoogleSpeechDataset(csv_file='./data_speech_commands_v0.02/training_set.csv', root_dir='.')
+    return GoogleSpeechDataset(input_file='./data_speech_commands_v0.02/train', root_dir='.', load_cached=True)
 
 
 def test_dataset():
-    return GoogleSpeechDataset(csv_file='./data_speech_commands_v0.02/test_set.csv', root_dir='.')
+    return GoogleSpeechDataset(input_file='./data_speech_commands_v0.02/test', root_dir='.', load_cached=True)
 
 
 def main():
--- task_setup.sh
+++ task_setup.sh
@@ -4,6 +4,8 @@ mkdir data_speech_commands_v0.02
 
 tar -xvf data_speech_commands_v0.02.tar.gz -C data_speech_commands_v0.02/
 
+pip install librosa
+
 python google_speech_data_loader.py
 
 mkdir log
